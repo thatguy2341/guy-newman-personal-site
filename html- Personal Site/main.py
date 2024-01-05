49cf8d6e322a9cc09f05097bb5974307b1a3c2ba
Guy Newman <gamenewman@gmail.com>
2024-01-05 10:47:29 +0200

    Added more self information, and fixed styling

--- html- Personal Site/main.py
+++ html- Personal Site/main.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
+
+@app.context_processor
+def get_year():
+    return dict(age=(datetime.now() - datetime(year=2005, month=8, day=10)).days // 360)
 
 @app.route('/')
 def hello_world():
     return render_template('main.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
