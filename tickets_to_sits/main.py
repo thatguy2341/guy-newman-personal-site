6c98a1fcc5a4c64f04b3c6324ee0ff978f988691
Guy Newman <gamenewman@gmail.com>
2025-01-08 13:07:13 +0200

    new project

--- tickets_to_sits/main.py
+++ tickets_to_sits/main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
+from dotenv import load_dotenv
 import requests
 
 app = Flask(__name__)
-
+load_dotenv(".env")
 if os.getenv("LOCAL") == "T":
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 else:
